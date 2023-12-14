a9c5c15a24d40b26345a2382cdf8fdfb4ae85115
unknown <kudashevtim@gmail.com>
2023-12-14 11:09:08 +0300

    range slider ready

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,145 @@
 
-const appInit = () => {
+const feInit = () => {
   switcher()
   stickySidebar()
   fastBtn()
   termSelect()
+  changeTab()
+  rangeSliderLoad()
+}
+
+const rangeSliderLoad = () => {
+  
+  if ($('.range-slider').length) {
+    var rangeSliders = $('.range-slider');
+    rangeSliderInit(rangeSliders);
+}
+}
+var rangeTimeout;
+
+const rangeSliderCreate = (slideIndex, slide) => {
+    var rangeSliderOptions;
+    var rangeSliderType = $(slide).attr('data-range-slider-type');
+    var rangeSliderStart = parseFloat($(slide).closest('.range-slider').find('.form-control').val());
+    var rangeSliderMin = parseFloat($(slide).closest('.range-slider').find('.form-control').data("min"));
+    var rangeSliderMax = parseFloat($(slide).closest('.range-slider').find('.form-control').data("max"));
+
+    if (rangeSliderType === 'weight') {
+        rangeSliderOptions = {
+            start: [rangeSliderStart],
+            range: {
+                'min': [rangeSliderMin, 1],
+                '28%': [15, 1],
+                '48%': [30, 5],
+                '60%': [100, 10],
+                '70%': [300, 50],
+                '80%': [1000, 100],
+                'max': [rangeSliderMax]
+            },
+            connect: 'lower'
+        }
+    } else if (rangeSliderType === 'volume') {
+        rangeSliderOptions = {
+            start: [rangeSliderStart],
+            range: {
+                'min': [rangeSliderMin, 0.01],
+                '40%': [0.5, 0.05],
+                '50%': [1, 0.1],
+                '76%': [3, 0.5],
+                '88%': [10, 1],
+                'max': [rangeSliderMax]
+            },
+            connect: 'lower'
+        }
+    }   else {
+      rangeSliderOptions = {
+          start: [1],
+          range: {
+              'min': [rangeSliderMin],
+              'max': [rangeSliderMax]
+          },
+          connect: 'lower'
+      }
+  }
+
+  $(slide).attr('data-range-slider-index', slideIndex);
+
+  var rangeSlidersItem = noUiSlider.create(slide.querySelector('.range-slider__ui'), rangeSliderOptions);
+
+  $(slide.querySelector('.range-slider__ui')).data('slider', rangeSlidersItem);
+
+  rangeSlidersItem.on('slide', function (values, handle) {
+      clearTimeout(rangeTimeout);
+      if (rangeSliderType === 'weight' || rangeSliderType === 'size' || rangeSliderType === 'height' || rangeSliderType === 'count') {
+          $(this.target).closest('.range-slider').find('.form-control').val(Math.round(values[handle]));
+      } else if (rangeSliderType === 'volume') {
+          $(this.target).closest('.range-slider').find('.form-control').val(parseFloat(values[handle]).toFixed(2) /* + ' Р СР’С–'*/ );
+      } else {
+          $(this.target).closest('.range-slider').find('.form-control').val(values[handle]);
+      }
+  });
+  rangeSlidersItem.on('change', function () {
+      var target = $(this.target);
+      rangeTimeout = setTimeout(function () {
+          target.closest('.range-slider').find('.form-control').trigger("change");
+      }, 500);
+  });
+
+}
+
+function rangeSliderInit(rangeSliders) {
+  for (var i = 0; i < rangeSliders.length; i++) {
+      rangeSliderCreate(i, rangeSliders[i]);
+  }
+
+  $('.range-slider__control').on('change', function () {
+      if ($(this).closest('.range-slider').attr('data-range-slider-type') === 'size' ) {
+          if (this.value.replace(/[^0-9]/g, '') > parseInt(this.getAttribute('data-max'))) {
+              this.value = parseInt(this.getAttribute('data-max'));
+          } else if (this.value.replace(/[^0-9]/g, '') < parseInt(this.getAttribute('data-min')) || this.value.replace(/[^0-9]/g, '') === '') {
+              this.value = parseInt(this.getAttribute('data-min'));
+          }
+          $(this).next().data('slider').set(parseInt(this.value.replace(/[^0-9]/g, '')));
+      }
+
+      if ($(this).closest('.range-slider').attr('data-range-slider-type') === 'weight' ) {
+          if (this.value.replace(/[^0-9]/g, '') > parseInt(this.getAttribute('data-max'))) {
+              this.value = parseInt(this.getAttribute('data-max'));
+             
+          } else if (this.value.replace(/[^0-9]/g, '') < parseInt(this.getAttribute('data-min')) || this.value.replace(/[^0-9]/g, '') === '') {
+              this.value = parseInt(this.getAttribute('data-min'));
+          }
+          $(this).next().data('slider').set(parseInt(this.value.replace(/[^0-9]/g, '')));
+        
+          if (parseInt($('[name="max_weight"]').val().replace(/[^0-9]/g, '')) > parseInt($('[name="weight"]').val().replace(/[^0-9]/g, ''))) {
+              $('[name="weight"]').val($('[name="max_weight"]').val())
+              $('[name="weight"]').next().data('slider').set(parseInt(this.value.replace(/[^0-9]/g, '')));
+          }
+
+      } else if ($(this).closest('.range-slider').attr('data-range-slider-type') === 'volume') {
+          if (parseFloat(this.value) > parseFloat(this.getAttribute('data-max'))) {
+              this.value = parseFloat(this.getAttribute('data-max'));
+          } else if (parseFloat(this.value) < parseFloat(this.getAttribute('data-min')) || parseFloat(this.value) === '') {
+              this.value = parseFloat(this.getAttribute('data-min'));
+          } if ( (!(parseFloat(this.value))) || (parseFloat(this.value) === 0)) {
+              this.value = parseFloat(this.getAttribute('data-min'));
+          }
+          $(this).next().data('slider').set(parseFloat(this.value /*.replace(/[^0-9]/g, '')*/ ).toFixed(2));
+      }
+  });
+}
+
+const changeTab = () => {
+  jQuery(".calctabs-js").on("click", "label:not(.active)", function () {
+    jQuery(this).addClass("active").siblings().removeClass("active")
+    jQuery(this)
+      .closest("div.calcitem_tabs-js")
+      .find("div.calcitem__content")
+      .find("div.calcform-js")
+      .removeClass("active")
+      .eq(jQuery(this).index())
+      .addClass("active")
+  })
 }
 
 const switcher = () => {
@@ -120,5 +256,5 @@ const stickySidebar = () => {
 
 
 jQuery(function (jQuery) {
-  appInit()
+  feInit()
 })
