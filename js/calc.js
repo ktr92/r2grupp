9fb524bb38f15bd832f2073f88258fedb800f0e7
unknown <kudashevtim@gmail.com>
2023-12-19 21:23:18 +0300

    step 3 desktop

--- js/calc.js
+++ js/calc.js
@@ -12,6 +12,444 @@ const feInit = () => {
   switchDepend()
 }
 
+const stepitems = {
+  properties: [],
+  radio: [],
+  set: [],
+}
+
+function checkExist(obj) {
+  const objfound = stepitems.properties.filter(
+      (item) => item.property === obj.property
+  )
+
+  if (objfound.length === 0) {
+      stepitems.properties.push(obj)
+  } else {
+      objfound[0].value = obj.value
+      objfound[0].text = obj.text
+  }
+}
+
+function checkExistRadio(obj) {
+  const objfound = stepitems.properties.filter(
+      (item) => item.property === obj.property
+  )
+
+  if (objfound.length === 0) {
+      stepitems.radio.push(obj)
+  } else {
+      objfound[0].value = obj.value
+      objfound[0].text = obj.text
+  }
+}
+
+
+function getInputData(selector) {
+  $(`[data-stepvisible="${selector}"]`)
+      .find(
+          'input[type="text"][data-steptype="source"], input[type="tel"][data-steptype="source"], input[type="email"][data-steptype="source"]'
+      )
+      .each(function (index) {
+          var obj = {}
+          obj.property = $(this).data("stepdata")
+          obj.value = $(this).val()
+          obj.text = ""
+          obj.type = "textinput"
+          checkExist(obj)
+      })
+}
+
+function getCheckboxData(selector) {
+  $(`[data-stepvisible="${selector}"]`)
+      .find('input[type="checkbox"][data-steptype="source"]')
+      .each(function (index) {
+          var obj = {}
+          obj.property = $(this).data("stepdata")
+          obj.value = $(this).is(":checked") ? 1 : 0
+          obj.text = $(this)
+              .parent()
+              .find("[data-text]")
+              .text()
+              .replace(/\s+/g, " ")
+          obj.type = "checkbox"
+          checkExist(obj)
+      })
+}
+
+function getRadioData(selector) {
+  $(`[data-stepvisible="${selector}"]`)
+      .find('input[type="radio"][data-steptype="source"]')
+      .each(function (index) {
+          var obj = {}
+          obj.property = $(this).data("stepdata")
+          obj.value = $(this).is(":checked") ? 1 : 0
+          obj.text = $(this).siblings("[data-text]").text().replace(/\s+/g, " ")
+          obj.type = "radio"
+          checkExist(obj)
+      })
+}
+
+function getSpanData(selector) {
+  $(`[data-stepvisible="${selector}"]`)
+      .find('span[data-steptype="source"]')
+      .each(function (index) {
+          var obj = {}
+          obj.property = $(this).data("stepdata")
+          obj.value = $(this).text().replace(/\s+/g, " ")
+          obj.text = ""
+          obj.type = "text"
+          checkExist(obj)
+      })
+}
+
+function initData() {
+  selectInit()
+  getInputData("anyway")
+  getCheckboxData("anyway")
+  getSpanData("anyway")
+  getSelectData("anyway")
+  getRadioData("anyway")
+
+  $('input[type="radio"][data-radiotab="tab"]:checked').each(function (
+      index
+  ) {
+      if ($(this).val()) {
+          var obj = {}
+          obj.property = $(this).attr("id")
+          obj.value = $(this).val()
+          obj.text = $(this).closest("label").text().replace(/\s+/g, " ")
+          obj.type = "radio"
+          checkExistRadio(obj)
+      }
+  })
+
+  stepitems.radio.forEach((item) => {
+      getInputData(item.property)
+      getCheckboxData(item.property)
+      getSpanData(item.property)
+      getSelectData(item.property)
+      getRadioData(item.property)
+  })
+
+  /*  console.log(stepitems) */
+}
+
+
+const fillStepData = (step) => {
+  if (step == 2) {
+      setData()
+      isSizeSet()
+  }
+
+  //confirm.html
+  if (step == 3) {
+      setData()
+  }
+
+  if (step == 4) {
+      $("#calcForm").prop("action", "/calc/save/")
+      $("input:checkbox, input:text").prop("disabled", false)
+      $("#calcForm").submit()
+  }
+}
+
+const getMailData = () => {
+  const $stepBlock = $('[data-entity="form-step-3"]')
+
+  let _html = ""
+  let from = ""
+  let to = ""
+  from += $stepBlock.find('.fromto__title[data-stepdata="f_otpravki"]').text()
+  from += $stepBlock
+      .find('.fromto__title[data-stepdata="f_otpravki"]')
+      .next("div")
+      .text()
+
+  to += $stepBlock.find('.fromto__title[data-stepdata="f_otpravki"]').text()
+  to += $stepBlock
+      .find('.fromto__title[data-stepdata="f_otpravki"]')
+      .next("div")
+      .text()
+
+  console.log("FROM")
+  console.log(from)
+  console.log("TO")
+  console.log(to)
+}
+
+function validateEmail() {
+  let valid = true
+  const email = []
+  const $email = $("input[type=email]:visible")
+  console.log("$email: ", $email)
+
+  $email.each(function () {
+      const obj = {
+          input: $(this),
+          val: $(this).val().toString(),
+      }
+      email.push(obj)
+  })
+
+  email.forEach(function (item) {
+      console.log("item: ", item)
+
+      var emailReg = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i
+
+      if (!emailReg.test(item.val)) {
+          valid = false
+          item.input.addClass("error")
+      } else {
+          valid = true
+          item.input.removeClass("error")
+      }
+  })
+  return valid
+}
+
+function validateRequired() {
+  let valid = true
+
+  valid = validateEmail()
+
+  const required = []
+  const $required = $('input[data-required="required"]:visible')
+
+  $required.each(function () {
+      const obj = {
+          input: $(this),
+          val: $(this).val().toString(),
+          min: +$(this).data("min") || 1,
+          max: +$(this).data("max") || 9999,
+      }
+      required.push(obj)
+  })
+
+  required.forEach(function (item) {
+      if (item.val.length < 1) {
+          valid = false
+          item.input.addClass("error")
+      }
+  })
+
+  if ($(".checkbox_required:visible").length) {
+      return false
+  }
+
+  if ($(".error").length) {
+      $("html, body").animate(
+          {
+              scrollTop: $(".error").offset().top - 50,
+          },
+          500
+      )
+  }
+
+  return valid
+}
+
+function validateDepending() {
+  let valid = true
+  const required = []
+  const $required = $("input[data-isrequired]:checked")
+
+  if ($required) {
+      $required.each(function () {
+          const obj = {
+              input: $(this),
+              val: $(this).val(),
+              depinput: $(this).data("isrequired"),
+          }
+          required.push(obj)
+      })
+  }
+
+  required.forEach(function (item) {
+      const depending = $(`[data-mbrequired="${item.depinput}"]`)
+      if (depending) {
+          if (!depending.val().length) {
+              valid = false
+              depending.addClass("error")
+          }
+      }
+  })
+
+  return valid
+}
+
+const checkConfirm = () => {
+  /*  console.log(123) */
+  if ($('input[name="privacy"]').is(":checked")) {
+      return true
+  } else {
+      $('input[name="privacy"]')
+          .closest(".pageform__check")
+          .find(".checkbox__box")
+          .addClass("error")
+      return false
+  }
+}
+
+const validateStepData = (step) => {
+  let validation = true
+  $("input").removeClass("error")
+
+  if (step == 2) {
+      let isTerminal = true
+      document
+          .querySelectorAll(".dropdownJS__label .dropdownJS__text1")
+          .forEach((item) => {
+              if (item.textContent) {
+                  item.closest(".dropdownJS__label").classList.remove("error")
+              } else {
+                  item.closest(".dropdownJS__label").classList.add("error")
+                  isTerminal = false
+              }
+          })
+
+      validation =
+          /* validateDepending() && */
+          validateRequired() && isTerminal
+  }
+
+  if (step == 3) {
+      validation = /* validateDepending() && */ validateRequired()
+  }
+  if (step == 4) {
+      validation = validateRequired() && checkConfirm()
+  }
+
+  return validation
+}
+
+const stepForm = () => {
+  $(document).on("click", '[data-entity="step-next"]', function (e) {
+      e.preventDefault()
+      if (validateStepData(currentStep + 1)) {
+          initData()
+          $(".form-step").hide()
+          ++currentStep
+          fillStepData(currentStep)
+          $('[data-entity="form-step-' + currentStep + '"]').show()
+          $([document.documentElement, document.body]).animate(
+              {
+                  scrollTop: $(
+                      '[data-entity="form-step-' + currentStep + '"]'
+                  ).offset().top,
+              },
+              1000
+          )
+          if (currentStep === 1) {
+              $('.form-step .calcpage__nav [data-entity="step-next"]').show()
+              $('[data-entity="step-next"]').text("РћС„РѕСЂРјРёС‚СЊ Р·Р°СЏРІРєСѓ")
+              $('[data-entity="step-prev"]').removeClass("active")
+          }
+          if (currentStep === 2) {
+              $('.form-step .calcpage__nav [data-entity="step-next"]').show()
+              $('[data-entity="step-next"]').text("РџСЂРѕРґРѕР»Р¶РёС‚СЊ")
+              $('[data-entity="step-prev"]').addClass("active")
+          }
+          if (currentStep === 3) {
+              $('[data-entity="step-next"]').text("РћС„РѕСЂРјРёС‚СЊ Р·Р°СЏРІРєСѓ")
+              $('[data-entity="step-prev"]').addClass("active")
+              $('.form-step .calcpage__nav [data-entity="step-next"]').hide()
+          }
+      }
+  })
+
+  $(document).on(
+      "click",
+      '[data-entity="step-prev"]',
+      function (e) {
+          e.preventDefault()
+          /* if (validateStepData(currentStep - 1)) { */
+          --currentStep
+          if (currentStep < 1) {
+              currentStep = 1
+          }
+          fillStepData(currentStep)
+          $(".form-step").hide()
+          $('[data-entity="form-step-' + currentStep + '"]').show()
+          $([document.documentElement, document.body]).animate(
+              {
+                  scrollTop: $(
+                      '[data-entity="form-step-' + currentStep + '"]'
+                  ).offset().top,
+              },
+              1000
+          )
+
+          if (currentStep === 1) {
+              $('[data-entity="step-prev"]').removeClass("active")
+          }
+          if (currentStep === 2) {
+              $('[data-entity="step-prev"]').addClass("active")
+          }
+          if (currentStep === 3) {
+              $('[data-entity="step-prev"]').addClass("active")
+          }
+      }
+      /*  } */
+  )
+}
+
+
+function setData() {
+  $('[data-steptype="result"]').each(function () {
+      const current = $(this).data("stepdata")
+      let input = stepitems.properties.filter(
+          (item) => item.property === current
+      )
+      if (
+          input.length &&
+          input[0].type === "textinput" &&
+          input[0].value != ""
+      ) {
+          $(this).text(input[0].value)
+      }
+      if (input.length && input[0].type === "checkbox" && input[0].value != 0) {
+          $(this).text(input[0].text)
+      }
+      if (input.length && input[0].type === "select" && input[0].value != "") {
+          $(this).text(input[0].value)
+      }
+      if (input.length && input[0].type === "text" && input[0].value != "") {
+          $(this).text(input[0].value)
+      }
+
+      let radio = stepitems.radio.filter((item) => item.property === current)
+      if (radio.length && radio[0].type === "radio" && radio[0].value != 0) {
+          $(this).text(radio[0].text)
+      }
+  })
+
+  $("[data-if]").each(function () {
+      const current = $(this).data("if")
+      let input = stepitems.properties.filter(
+          (item) => item.property === current
+      )
+      if (
+          input.length &&
+          input[0].type === "textinput" &&
+          input[0].value != ""
+      ) {
+          $(this).show()
+      } else {
+          $(this).hide()
+      }
+  })
+
+  $('[data-totaltype="fizyur"]').each(function () {
+      const current = $(this).data("stepdata")
+      if (stepitems.radio.filter((item) => item.property === current).length) {
+          $(this).show()
+      } else {
+          $(this).hide()
+      }
+  })
+}
+
 const autoCalc = () => {
 
   const calcOver = function (e) {
