c58567d42f2181f5b905d5e750953e8449d3e02a
unknown <kudashevtim@gmail.com>
2023-12-29 16:48:15 +0300

    sb popup

--- js/restyle.js
+++ js/restyle.js
@@ -8,4 +8,46 @@ $(document).ready(function() {
     e.preventDefault()
     $('.contacts-nav-column').slideToggle()
   })
+
+  $('.cityInput_tab').on('input', function(e) {
+    $('.caclform__popup').addClass('active')
+  })
+
+
+    jQuery("[data-tab]").on("click", function () {
+      jQuery(this).addClass("active").siblings().removeClass("active")
+      jQuery(this)
+        .closest("[data-tabs]")
+        .find("[data-tabcontent]")
+        .removeClass("active")
+        .eq(jQuery(this).index())
+        .addClass("active")
+    })
+
+
+    function closeByClickOutside(element, button, callback) {
+      $(document).click(function(event) {
+          if (!$(event.target).closest(`${element},${button}`).length) {
+              $(button).removeClass('active')
+              $(element).removeClass('active')
+              // or  
+              //$(element).hide()
+          }
+      });
+  
+      $(document).keyup(function(e) {
+          if (e.key === "Escape") { // escape key maps to keycode `27`
+              $(button).removeClass('active')
+              $(element).removeClass('active')
+              // or  
+              //$(element).hide()
+          }
+      });
+  
+      if (callback instanceof Function) { callback(); }
+    }
+  
+  // usage
+  closeByClickOutside('.caclform__popup', '[name="from_text_tab1"]')
+
 })
